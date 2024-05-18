dc286ca1ebf54d42c6b51cb53bccaf7afd07c7d8
afarut <renat030205@ya.ru>
2024-05-18 13:32:50 +0300

    baseline

--- model.py
+++ model.py
@@ -3,23 +3,58 @@ from custom_rubert import RubertTinyClassifier
 from config import TOKENIZER_PATH, MODEL_PATH
 from transformers import AutoTokenizer
 import json
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 
 class ResourceClassifier:
 	def __init__(self):
-		if torch.cuda.is_avalable():
-			device = "cuda"
+		if torch.cuda.is_available():
+			self.device = "cuda"
 		else:
-			device = "cpu"
+			self.device = "cpu"
 
-		self.tokenizer = AutoTokenizer(TOKENIZER_PATH)
+		self.tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH)
 		
-		self.rubert = RubertTinyClassifier(2309).to(device).eval()
+		self.rubert = RubertTinyClassifier(2309).to(self.device).eval()
 		self.rubert.load_state_dict(torch.load(MODEL_PATH))
 		
 		with open('classes.json', "r", encoding="utf-8") as f:
 		    self.classes = json.load(f)
 
+		self.data = pd.read_csv("./datasets/bert_embeds.csv")
+
 	def __call__(self, batch: list):
-		data = self.tokenizer(batch)
-		self.rubert
+		if type(batch) != list:
+			raise Exception("В качестве аргумента передайте список")
+
+		data = self.tokenizer(batch, padding=True, truncation=True, return_tensors='pt').to(self.device)
+		with torch.inference_mode():
+			logits = self.rubert(**data)
+			ids = logits.argmax(dim=1)
+			embends = self.rubert.bert(**data).pooler_output.tolist()
+		titles = [self.classes[i] for i in ids]
+		result = []
+		for j in range(len(titles)):
+			rows = self.data.loc[self.data["Название кластера"] == titles[j]]
+			row_value = -1
+			row_index = -1
+			for i in rows.index:
+				if row_value < cosine_similarity([eval(rows["embeds"][i])], [embends[j]])[0][0]:
+					row_value = cosine_similarity([eval(rows["embeds"][i])], [embends[j]])[0][0]
+					row_index = i
+			if row_value < 0:
+				result.append({"status": "error", "text": "Невозможно присвоить"}) 
+			else:
+				result.append({"status": "ok", "result": {"label": self.data["Наименование"][row_index], "probability": row_value, "group": titles[j]}})
+		return result
+
+
+
+
+if __name__ == "__main__":
+	model = ResourceClassifier()
+	print("-----------------Start-----------------")
+	while True:
+		print(model([input(), "dawdwa"]))
